4a92d8fec8729854adda8fb4562a3d44d7cc3a6a
LeandroC192 <Leandroc16@nycstudents.net>
2025-05-02 01:04:35 +0000

    Completed code

--- WordPair.java
+++ WordPair.java
@@ -1,13 +1,32 @@
 public class WordPair {
     /** Constructs a WordPair object. */
-    public WordPair(String first, String second) {
-        /* implementation not shown */ }
+    String first1 = "";
+    String second2 = "";
+
+    public WordPair(String first, String second) 
+    {
+        first1 = first;
+        second2 = second;
+        /* implementation not shown */ 
+    }
 
     /** Returns the first string of this WordPair object. */
-    public String getFirst() {
-        /* implementation not shown */ }
+    public String getFirst() 
+    {
+        return first1;
+        /* implementation not shown */ 
+    }
 
     /** Returns the second string of this WordPair object. */
-    public String getSecond() {
-        /* implementation not shown */ }
+    public String getSecond() 
+    {
+        return second2;
+        /* implementation not shown */ 
+    }
+    
+    public String toString()
+    {
+        return "(\"" + first1 + "\", \"" + second2 + "\")";
+    }
+
 }
